4158556bfee8cfbdb59749961ffa686b330f7c6d
Dorothy <dorothy@anttoweb.com>
2020-05-12 09:38:51 +0800

    秀米，135编辑器

--- src/assets/ueditor/ueditor.config.js
+++ src/assets/ueditor/ueditor.config.js
@@ -343,6 +343,9 @@
 
         //webAppKey 百度应用的APIkey，每个站长必须首先去百度官网注册一个key后方能正常使用app功能，注册介绍，http://app.baidu.com/static/cms/getapikey.html
         //, webAppKey: ""
+
+        //抓取远程图片是否开启
+        ,catchRemoteImageEnable:true
     };
 
     function getUEBasePath(docUrl, confUrl) {
